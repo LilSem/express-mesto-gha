971b7d1209d4a313226aef72526d977aa4a48b54
LilSem <evgenyi_sit@mail.ru>
2023-04-19 23:02:37 +0300

    feat:creat routers, controllers, models

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { NotFoundError } = require('./errors/errorsExport');
+const { NotFoundError } = require("./errors/errorsExport");
 
 const app = express();
 
